2a0a558056ce0066f0c60b6d90ce11b6b49ac023
odinhufnagl <odin.hufnagl@gmail.com>
2022-11-04 12:48:07 +0100

    simple bottom modal

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,8 @@
+import BottomModal from './BottomModal/BottomModal';
 import Button from './Button/Button';
 import Container from './Container/Container';
 import Input from './Input/Input';
 import Modal from './Modal/Modal';
 import Spacer from './Spacer/Spacer';
 import Text from './Text/Text';
-export { Button, Text, Spacer, Container, Input, Modal };
+export { Button, Text, Spacer, Container, Input, Modal, BottomModal };
