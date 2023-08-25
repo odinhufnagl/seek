a452b2fe4b85dc01fe9529b0bdefc87d95d38027
odinhufnagl <odin.hufnagl@gmail.com>
2023-08-25 14:06:40 +0200

    better error handling

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -1,17 +1,19 @@
-import Geolocation, { GeolocationResponse } from '@react-native-community/geolocation';
+import Geolocation, {
+  GeolocationError,
+  GeolocationResponse,
+} from '@react-native-community/geolocation';
 import { Permission } from '../constants';
 import { hasPermission } from './permission';
 
-export const getGeoLocation = async (onSuccess: (location: GeolocationResponse) => void) => {
+export const getGeoLocation = async (
+  onSuccess: (location: GeolocationResponse) => void,
+  onError: (e: GeolocationError) => void,
+) => {
   if (!(await hasPermission(Permission.LOCATION))) {
     return;
   }
   const config = {
     enableHighAccuracy: false,
   };
-  Geolocation.getCurrentPosition(
-    (location) => onSuccess(location),
-    (e) => console.log(e),
-    config,
-  );
+  Geolocation.getCurrentPosition((location) => onSuccess(location), onError, config);
 };
