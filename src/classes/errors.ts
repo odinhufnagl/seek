271fa8804e588954ff826811f263d5990eaeb888
odinhufnagl <odin.hufnagl@gmail.com>
2023-08-25 14:06:40 +0200

    better error handling

--- src/classes/errors.ts
+++ src/classes/errors.ts
@@ -1,14 +1,43 @@
+import { ApiErrorType } from '../types';
+
 export class ApiError extends Error {
   status: number;
-  constructor(status: number, message: string) {
+  errorCode: ApiErrorType;
+  constructor(status: number, errorCode: ApiErrorType, message: string) {
     super(message);
     this.name = 'ApiError';
+    this.errorCode = errorCode;
     this.status = status;
   }
 }
-export class NetworkError extends Error {
-  constructor(message: string) {
+
+export class AppError extends Error {
+  constructor(message?: string) {
     super(message);
-    this.name = 'NetworkError';
+  }
+  static fromApiError(e: ApiError) {
+    switch (e.errorCode) {
+      case 'E203':
+        return new EmailInUseError();
+      case 'E101':
+        return new WrongEmailPasswordError();
+      default:
+        return new AppError('Something went wrong');
+    }
+  }
+}
+export class EmailInUseError extends AppError {
+  constructor() {
+    super('Email already in use');
+  }
+}
+export class WrongEmailPasswordError extends AppError {
+  constructor() {
+    super('Wrong email or password');
+  }
+}
+export class NetworkError extends AppError {
+  constructor() {
+    super('Network error');
   }
 }
