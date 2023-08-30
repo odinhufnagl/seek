5af336863a8ed153a3c5338b28e22c7aad75f158
odinhufnagl <odin.hufnagl@gmail.com>
2023-08-30 16:36:51 +0200

    update location

--- src/screens/EditProfileScreen/index.tsx
+++ src/screens/EditProfileScreen/index.tsx
@@ -2,16 +2,17 @@ import React, { useEffect, useState } from 'react';
 
 import { useRoute } from '@react-navigation/native';
 import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { Button, Container, Icon, Input, Loading, Spacer } from '../../common';
 import { Header, ModalList } from '../../components';
 import { DEFAULT_IMAGES, DIMENS } from '../../constants';
-import { useFetchUser, useTheme } from '../../hooks';
+import { useFetchUser, useSearchCities, useTheme } from '../../hooks';
 import { translate } from '../../i18n';
 import { useAuth } from '../../providers/AuthProvider';
 import { openCamera, openLibrary, updateUser, uploadProfileImageFile } from '../../services';
-import { FileInfo, NavigationProps, Theme } from '../../types';
+import { locationByPlaceID } from '../../services/maps';
+import { FileInfo, NavigationProps, Theme, UserModel } from '../../types';
 import { extractFileTypeFromFilename, showSnackbar } from '../../utils';
-
 type Props = {
   navigation: NavigationProps;
 };
@@ -23,6 +24,7 @@ const EditProfileScreen = ({ navigation }: Props) => {
   const { refetch } = params as Params;
   const { theme } = useTheme();
   const { currentUser, refetchCurrentUser } = useAuth();
+  const [locationSearchQuery, setLocationSearchQuery] = useState('');
   const translateKey = 'editProfileScreen.';
   const { data: user, isLoading } = useFetchUser(currentUser?.id);
   const [name, setName] = useState(user?.name || '');
@@ -30,8 +32,13 @@ const EditProfileScreen = ({ navigation }: Props) => {
   const [isSavingLoading, setIsSavingLoading] = useState(false);
   const [imageModalVisible, setImageModalVisible] = useState(false);
   const [isDefaultProfileImage, setIsDefaultProfileImage] = useState(false);
-
+  const [selectedPlaceId, setselectedPlaceId] = useState(null);
   const [newProfileImagePath, setNewProfileImagePath] = useState<string>();
+  const { data: locationSearches } = useSearchCities(locationSearchQuery);
+
+  useEffect(() => {
+    console.log('locationSearched', locationSearches);
+  }, [locationSearches]);
 
   const handleSavePress = async () => {
     if (!currentUser) {
@@ -40,6 +47,8 @@ const EditProfileScreen = ({ navigation }: Props) => {
     try {
       setIsSavingLoading(true);
       // TODO: this is done twice, both here and onboarding, should be simplified into one service
+
+      const newUserData: Partial<UserModel> = { name, bio };
       if (newProfileImagePath && !isDefaultProfileImage) {
         const fileInfo: FileInfo = {
           name: newProfileImagePath.split('/').pop() || 'image.png',
@@ -47,19 +56,19 @@ const EditProfileScreen = ({ navigation }: Props) => {
           type: extractFileTypeFromFilename(newProfileImagePath),
         };
         const profileImageUrl = await uploadProfileImageFile(fileInfo);
-        await updateUser(currentUser?.id, { name, bio, profileImage: { url: profileImageUrl } });
-      } else {
-        if (newProfileImagePath && isDefaultProfileImage) {
-          await updateUser(currentUser.id, {
-            name,
-            bio,
-            profileImage: { url: newProfileImagePath },
-          });
-        } else {
-          await updateUser(currentUser.id, { name, bio });
-        }
+        newUserData.profileImage = { url: profileImageUrl };
+      }
+      if (newProfileImagePath && isDefaultProfileImage) {
+        newUserData.profileImage = { url: newProfileImagePath };
+      }
+      if (selectedPlaceId) {
+        console.log('selectedPlaceId', selectedPlaceId);
+        const newLocation = await locationByPlaceID(selectedPlaceId);
+        console.log('newLocation', newLocation);
+        newUserData.location = { coordinate: newLocation.coordinate };
       }
 
+      await updateUser(currentUser.id, newUserData);
       setIsSavingLoading(false);
       refetchCurrentUser();
       refetch();
@@ -101,43 +110,69 @@ const EditProfileScreen = ({ navigation }: Props) => {
       />
       <Container>
         <>
-          <View style={{ flex: 1 }}>
-            <TouchableOpacity
-              style={{ width: 100, height: 100, alignSelf: 'center' }}
-              onPress={() => setImageModalVisible(true)}
-            >
-              <Image
-                style={styles(theme).profileImage}
-                source={{ uri: newProfileImagePath || user?.profileImage?.url }}
+          <KeyboardAwareScrollView
+            showsVerticalScrollIndicator={false}
+            keyboardShouldPersistTaps='always'
+          >
+            <View style={{ flex: 1 }}>
+              <TouchableOpacity
+                style={{ width: 100, height: 100, alignSelf: 'center' }}
+                onPress={() => setImageModalVisible(true)}
+              >
+                <Image
+                  style={styles(theme).profileImage}
+                  source={{ uri: newProfileImagePath || user?.profileImage?.url }}
+                />
+                <Icon
+                  icon='write'
+                  style={{
+                    width: 30,
+                    height: 30,
+                    position: 'absolute',
+                    bottom: 0,
+                    right: 0,
+                    backgroundColor: theme.base.high,
+                    borderColor: theme.brand,
+                    borderWidth: 1,
+                  }}
+                  fill={theme.brand}
+                  size={15}
+                />
+              </TouchableOpacity>
+              <Spacer spacing='extraLarge' />
+              <Input
+                value={name}
+                updateValue={setName}
+                variant='fourth'
+                title='Name'
+                autoCapitalize='words'
               />
-              <Icon
-                icon='write'
-                style={{
-                  width: 30,
-                  height: 30,
-                  position: 'absolute',
-                  bottom: 0,
-                  right: 0,
-                  backgroundColor: theme.base.high,
-                  borderColor: theme.brand,
-                  borderWidth: 1,
-                }}
-                fill={theme.brand}
-                size={15}
-              />
-            </TouchableOpacity>
-            <Spacer spacing='extraLarge' />
-            <Input
-              value={name}
-              updateValue={setName}
-              variant='fourth'
-              title='Name'
-              autoCapitalize='words'
-            />
-            <Spacer />
+              <Spacer />
 
-            <Input multiline value={bio} updateValue={setBio} variant='fourth' title='Bio' />
-          </View>
+              <Input multiline value={bio} updateValue={setBio} variant='fourth' title='Bio' />
+              <Spacer />
+              <Input
+                placeholder={`${user?.location?.cityName}, ${user?.location?.country.name}`}
+                variant='fourth'
+                title='Location'
+                value={locationSearchQuery}
+                updateValue={setLocationSearchQuery}
+                rightIcon={
+                  locationSearchQuery?.length > 0
+                    ? { icon: 'close', size: 14, onPress: () => setLocationSearchQuery('') }
+                    : undefined
+                }
+                dropdown
+                dropdownItems={locationSearches?.map((ls) => ({
+                  title: ls.title,
+                  value: ls.placeId,
+                }))}
+                dropdownValue={selectedPlaceId}
+                updateDropdownValue={setselectedPlaceId}
+              />
+            </View>
+          </KeyboardAwareScrollView>
+          <Spacer />
           <Button
             title='Save'
             onPress={handleSavePress}
