db6e57e0284bd578318462e2cffeef23a14581e7
odinhufnagl <odin.hufnagl@gmail.com>
2023-09-17 15:10:55 +0200

    same color on default iamge

--- src/constants/externalImages.ts
+++ src/constants/externalImages.ts
@@ -1,4 +1,4 @@
 export default {
-  letter: (name) =>
-    `https://ui-avatars.com/api/?size=512&name=${name}&length=1&background=random&format=jpg`,
+  letter: (name, color) =>
+    `https://ui-avatars.com/api/?size=512&name=${name}&length=1&background=${color}&format=jpg`,
 };
