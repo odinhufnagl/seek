9c7c0f2797c443db39d614436ecf63440bc26d04
odinhufnagl <odin.hufnagl@gmail.com>
2023-09-27 18:06:56 +0200

    fix location error

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -3,13 +3,20 @@ import { Permission } from '../constants';
 import { hasPermission } from './permission';
 
 export const getGeoLocation = async (onSuccess: (location) => void, onError: (e) => void) => {
-  if (!(await hasPermission(Permission.LOCATION))) {
+  if (
+    !(
+      (await hasPermission(Permission.FINE_LOCATION)) ||
+      (await hasPermission(Permission.COARSE_LOCATION))
+    )
+  ) {
     return;
   }
   const config = {
     enableHighAccuracy: false,
     timeout: 15000,
     maximumAge: 10000,
+    showLocationDialog: true,
+    forceRequestLocation: true,
   };
   Geolocation.getCurrentPosition((location) => onSuccess(location), onError, config);
 };
